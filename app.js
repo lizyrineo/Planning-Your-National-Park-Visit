8cf6cf84cc3f5cfd75ac264f2c2c9f0fa8eeb6ae
Liz Yrineo <lizyrineo@Lizs-MacBook-Pro.local>
2020-11-14 21:12:39 -0700

    format list complete

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ async function getState(event) {
   console.log(state)
   try {
     const stateParkURL = `https://developer.nps.gov/api/v1/parks?stateCode=${state}&api_key=gvphhiHHG97vMhTNAeRfJpBHEPlctO6jPByPu0ji`;
+    // removePark()
     let response = await axios.get(stateParkURL)
     let parkList = response.data.data
     createList(parkList)
@@ -23,16 +24,47 @@ function createList(list) {
     console.log(park)
     li.textContent = park.fullName
     ul.append(li)
+    li.addEventListener("click", () => {
+      getParkData(park)
+      
+    })
   })
 }
+
 form.addEventListener('submit', getState);
 
 function clicked() {
   console.log('click');
   
 }
-let h2 = document.querySelector('h2');
-h2.addEventListener('click', clicked)
+let li = document.querySelector('li');
+
+
+function getParkData(park) {
+  
+  let div = document.createElement('div')
+    let p = document.createElement('p')
+    console.log(park)
+    p.textContent = park.description
+  div.append(p)
+  const parkInfo = document.querySelector("#park-info");
+  parkInfo.append(div);
+}
+
+// function getImage(img) {
+//   let img = document.createElement('img')
+// image = park.images
+  // 
+  
+// }
+
+
+
+
+
+
+
+
 
 
 
